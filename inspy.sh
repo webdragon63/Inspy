411b09ce3d9bcc70e14f2085382ae27da1a4cec1
WebDragon <117004849+webdragon63@users.noreply.github.com>
2024-11-03 15:19:28 -0500

    Update inspy.sh

--- inspy.sh
+++ inspy.sh
@@ -5,21 +5,24 @@ if [[ $(id -u) -ne 0 ]] ; then
    echo "You are Not Root! Please Run as Root" ; exit 1 ; 
 else echo "Checking For Requirement Packages.." ; 
 fi
-
 clear
-cat<< EOF
+VERMILION='\033[0;31m'
+WHITE='\033[0;37m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
 
-                              ░▀█▀░█▀█░█▀▀░█▀█░█░█
-                              ░░█░░█░█░▀▀█░█▀▀░░█░
-                              ░▀▀▀░▀░▀░▀▀▀░▀░░░░▀░
+echo -e "${VERMILION}                              ░▀█▀░█▀█░█▀▀░█▀█░█░█"
+echo -e "${WHITE}                              ░░█░░█░█░▀▀█░█▀▀░░█░"
+echo -e "${GREEN}                              ░▀▀▀░▀░▀░▀▀▀░▀░░░░▀░"
                                                                                                                 
- ---------ＡＮ  ＡＤＶＡＮＣＥ  ＤＡＲＫＷＥＢ  ＯＳＩＮＴ  ＴＯＯＬ----------
+echo -e "${BLUE} ---------ＡＮ  ＡＤＶＡＮＣＥ  ＤＡＲＫＷＥＢ  ＯＳＩＮＴ  ＴＯＯＬ----------"
                                
-                           MADE BY: INDIAN CYBER ARMY               
-           YOUTUBE CHANNEL: https://www.youtube.com/@indiancyberarmy5 
---------------------------------------------------------------------------------     
+echo -e "${WHITE}                           MADE BY: INDIAN CYBER ARMY "              
+echo -e "${CYAN}           YOUTUBE CHANNEL: https://www.youtube.com/@indiancyberarmy5" 
+echo -e "${CYAN}--------------------------------------------------------------------------------"     
          
-EOF
+echo -e "${BLUE}"
 read -p " Do you want to start tor service
 a) yes
 b) no (Default is b)  " ab
@@ -32,16 +35,16 @@ sleep 2
 
 esac
 clear
-cat<< EOF
-
-                              ░▀█▀░█▀█░█▀▀░█▀█░█░█
-                              ░░█░░█░█░▀▀█░█▀▀░░█░
-                              ░▀▀▀░▀░▀░▀▀▀░▀░░░░▀░
+echo -e "${VERMILION}                              ░▀█▀░█▀█░█▀▀░█▀█░█░█"
+echo -e "${WHITE}                              ░░█░░█░█░▀▀█░█▀▀░░█░"
+echo -e "${GREEN}                              ░▀▀▀░▀░▀░▀▀▀░▀░░░░▀░"
                                                                                                                 
- ---------ＡＮ  ＡＤＶＡＮＣＥ  ＤＡＲＫＷＥＢ  ＯＳＩＮＴ  ＴＯＯＬ----------
- 
- 
-EOF
+echo -e "${BLUE} ---------ＡＮ  ＡＤＶＡＮＣＥ  ＤＡＲＫＷＥＢ  ＯＳＩＮＴ  ＴＯＯＬ----------"
+                               
+echo -e "${WHITE}                           MADE BY: INDIAN CYBER ARMY "              
+echo -e "${CYAN}           YOUTUBE CHANNEL: https://www.youtube.com/@indiancyberarmy5" 
+echo -e "${CYAN}--------------------------------------------------------------------------------" 
+echo -e "${BLUE}" 
 read -p "a) Scrape images
 b) Crawl website 
 (Enter a or b)  :" ab
@@ -55,21 +58,20 @@ esac
 python3 src/imscr.py
 
 clear
-cat<< EOF
-
-                              ░▀█▀░█▀█░█▀▀░█▀█░█░█
-                              ░░█░░█░█░▀▀█░█▀▀░░█░
-                              ░▀▀▀░▀░▀░▀▀▀░▀░░░░▀░
+echo -e "${VERMILION}                              ░▀█▀░█▀█░█▀▀░█▀█░█░█"
+echo -e "${WHITE}                              ░░█░░█░█░▀▀█░█▀▀░░█░"
+echo -e "${GREEN}                              ░▀▀▀░▀░▀░▀▀▀░▀░░░░▀░"
                                                                                                                 
- ---------ＡＮ  ＡＤＶＡＮＣＥ  ＤＡＲＫＷＥＢ  ＯＳＩＮＴ  ＴＯＯＬ----------
- 
- 
-EOF
-
+echo -e "${BLUE} ---------ＡＮ  ＡＤＶＡＮＣＥ  ＤＡＲＫＷＥＢ  ＯＳＩＮＴ  ＴＯＯＬ----------"
+                               
+echo -e "${WHITE}                           MADE BY: INDIAN CYBER ARMY "              
+echo -e "${CYAN}           YOUTUBE CHANNEL: https://www.youtube.com/@indiancyberarmy5" 
+echo -e "${CYAN}--------------------------------------------------------------------------------" 
+echo -e "${BLUE}" 
 read -p "Do you want to craete a powershell backdoor using villain
 y) Yes I Want
-n) No Not Needed
-[choose any option y or n (Default is y)] :" yn
+Press Ctrl+C for Exit
+[choose any option y or Ctrl+C (Default is y)] :" yn
 case $yn in
     y) echo Starting Villain ...;;
     n) echo Exiting directly ...;
